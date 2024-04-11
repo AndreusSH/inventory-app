b0d22b08f5ff8d6d6e10135a91cab086935f9a60
AndreusSH <andreamammoliti@yahoo.it>
2024-04-11 11:54:21 +0200

    setting views

--- server.js
+++ server.js
@@ -1,9 +1,17 @@
-const express = require('express');  
+const express = require('express'); 
+const path = require('path')
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
+
+app.set('views', path.join(__dirname, 'views')); 
+// Set EJS as the template engine
+app.set('view engine', 'ejs');
+
+// Middleware to parse incoming request bodies
+app.use(express.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
-  res.send('Hello, Express.js!');
+  res.render('home');
 });
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
