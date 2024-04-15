fcc78518993560398fe982e8c2c753dbfbce36b5
AndreusSH <andreamammoliti@yahoo.it>
2024-04-15 19:07:39 +0200

    routes

--- routes/products.js
+++ routes/products.js
@@ -7,6 +7,17 @@ const productController = require('../controllers/productController')
 router.get('/', productController.homepage);
 router.get('/new', productController.newProduct);
 router.post('/new', productController.postProduct);
+router.get('/products', productController.getProducts);
+router.get('/view/:id', productController.viewProduct);
+router.get('/edit/:id', productController.editProduct);
+router.post('/edit/:id', productController.updateProduct);
+router.get('/delete/:id', productController.deleteProductPage);
+router.post('/delete/:id', productController.deleteProduct);
+
+
+
+
+
 
 
 
